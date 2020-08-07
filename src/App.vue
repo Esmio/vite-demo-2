fc863f3cc152752ce9218f424d57b0a122cf2f0f
dongxiaoming <dongxiaoming@jiuxiniot.com>
2020-08-07 17:24:08 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>count is {{count}}</span><br>
+    <span>object.n is {{object.n}}</span><br>
+    <button @click="add">Add 1</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+export default {
+  setup() {
+    const count = ref(0);
+    const object = reactive({ n: 999 })
+    setTimeout(() => {
+      count.value += 100
+      object.n += 1000
+    }, 3000)
+    return {
+      count,
+      object
+    }
+  },
+  methods: {
+    add() {
+      this.count += 1
+      this.object.n += 1
+    }
+  }
+}
+</script>
+count
